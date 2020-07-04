412d33417551342a188b3edb81724ce19d712164
lml <soalin228@163.com>
2020-07-04 18:05:30 +0800

    feat: global error handle

--- src/common/ErrorHandle.js
+++ src/common/ErrorHandle.js
@@ -2,15 +2,28 @@
  * User: soalin
  * Date: 2020/7/4
  * Time: 16:06
- * Desc: 鉴权中间件
+ * Desc: 异常处理中间件
  */
 export default (ctx, next) => {
   return next().catch((err) => {
+    // 鉴权异常处理
     if (401 === err.status) {
       ctx.status = 401
-      ctx.body = 'Protected resource, use Authorization header to get access\n'
+      ctx.body = {
+        code: 401,
+        msg: 'Protected resource, use Authorization header to get access\n'
+      }
     } else {
-      throw err
+      // 错误异常处理
+      ctx.status = err.status || 500
+      ctx.body = {
+        code: 500,
+        msg: err.message
+      }
+      // 开发环境打印错误所在行数
+      if (process.env.NODE_ENV === 'development') {
+        console.error(err.stack)
+      }
     }
   })
 }
