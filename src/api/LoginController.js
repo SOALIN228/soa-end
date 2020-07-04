7a49f67cb21cd35725c1d5ae49d5864e0ef6410e
lml <soalin228@163.com>
2020-07-04 17:31:58 +0800

    feat: login api

--- src/api/LoginController.js
+++ src/api/LoginController.js
@@ -2,6 +2,8 @@ import send from '@/config/MailConfig'
 import moment from 'moment'
 import jsonwebtoken from 'jsonwebtoken'
 import config from '@/config'
+import { checkCode } from '@/common/Utils'
+import User from '@/model/User'
 
 class LoginController {
   async forget (ctx) {
@@ -26,14 +28,41 @@ class LoginController {
   }
 
   async login (ctx) {
-    let token = jsonwebtoken.sign({
-      _id: 'soalin'
-    }, config.JWT_SECRET, {
-      expiresIn: '1d'
-    })
-    ctx.body = {
-      code: 200,
-      token
+    const { body } = ctx.request
+    const { username, password, sid, code } = body
+    // 图像验证码是否有效
+    const isValid = await checkCode(sid, code)
+    if (isValid) {
+      // 验证用户名密码
+      let checkUserPassword = false
+      let user = await User.findOne({ username })
+      if (user.password === password) {
+        checkUserPassword = true
+      }
+      if (checkUserPassword) {
+        // 验证通过，返回token
+        let token = jsonwebtoken.sign({
+          _id: username
+        }, config.JWT_SECRET, {
+          expiresIn: '1d'
+        })
+        ctx.body = {
+          code: 200,
+          token
+        }
+      } else {
+        // 用户名或密码验证失败
+        ctx.body = {
+          code: 404,
+          msg: '用户名或密码错误'
+        }
+      }
+    } else {
+      // 图片验证码验证失败
+      ctx.body = {
+        code: 401,
+        msg: '图片验证码不正确，请检查！'
+      }
     }
   }
 }
---
+++ src/common/Utils.js
@@ -0,0 +1,20 @@
+/**
+ * User: soalin
+ * Date: 2020/7/4
+ * Time: 16:33
+ * Desc: 接口通用方法
+ */
+import { getValue } from '@/config/RedisConfig'
+
+const checkCode = async (key, value) => {
+  const redisData = await getValue(key)
+  if (redisData !== null) {
+    return redisData.toLowerCase() === value.toLowerCase()
+  } else {
+    return false
+  }
+}
+
+export {
+  checkCode
+}
